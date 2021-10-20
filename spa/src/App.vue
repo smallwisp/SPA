c5b6e823c281abb6281d4d3b5f13a29a2b0582ec
smallwisp <4ubarov96@mail.ru>
2021-10-20 22:53:00 +0300

    created base template of vue application

---
+++ spa/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  body {
+    background: linear-gradient(45deg, rgba(16, 17, 15, 0.815) 0%, #0b3102cb 85.62%), url('./img/crypto-bg.jpg') no-repeat center top / cover;
+    
+  }
+</style>
