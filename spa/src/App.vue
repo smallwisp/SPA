ff85a9f5184915a407254c4a7dfa45df33e55679
smallwisp <4ubarov96@mail.ru>
2021-10-27 01:20:55 +0300

    added binding currency__input and created graph block

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -1,31 +1,63 @@
 <template>
   <div id="app">
     <div class="header">
-      <h1 class="header-converter"><a href="#" class="converter-link">{{converter}}</a></h1>
-      <h1 class="header-briefcase"><a href="#" class="briefcase-link">{{briefcase}}</a></h1>
+      <div class="header__container container">
+        <h1 class="header__converter"><a href="#" class="header__link">{{converter}}</a></h1>
+        <h1 class="header__briefcase"><a href="#" class="header__link">{{briefcase}}</a></h1>
+      </div>
     </div>
+    <!-- /.header -->
     <div class="currency container">
-      <div class="currency-block first">
-        <span class="currency-choose">Выберите валюту</span>
-        <select name="" id="" class="select currency">
+      <div class="currency__block currency__block_first">
+        <span class="currency__choose">Выберите валюту</span>
+        <select name="" id="" class="currency__select">
           <option value="">Bitcoin (BTC)</option>
           <option value="">Ethereum (ETH)</option>
           <option value="">US dollar (USD)</option>
-      </select>
-      <input class="currency-input" type="text" placeholder="Введите сумму">
+        </select>
+        <input
+          v-bind:value="currencyInput"
+          @input="currencyInput = $event.target.value"
+          class="currency__input" 
+          type="text"
+          placeholder="Введите сумму">
       </div>
-      <div class="currency-block second">
-        <span class="currency-choose">Выберите валюту</span>
-        <select name="" id="" class="select currency">
+      <div class="currency__block second">
+        <span class="currency__choose">Выберите валюту</span>
+        <select name="" id="" class="currency__select"
+          v-bind:value="currencySelect"
+          @change="currencySelect = $event.target.value"
+        >
           <option value="">Bitcoin (BTC)</option>
           <option value="">Ethereum (ETH)</option>
           <option value="">US dollar (USD)</option>
       </select>
-      <input class="currency-input" type="text" placeholder="0.00" disabled="">
+      <input 
+        v-bind:value="currencyOutput"
+        class="currency__input currency__output" 
+        type="text"
+        placeholder="0.00" 
+        disabled="">
       </div>
     </div>
-    <router-view/>
+    <!-- /.currency -->
+    <div class="graph container">
+      <div class="graph__heading"><h2 class="graph__text">График изменения курса криптовалюты за последние 14 дней</h2></div>
+      <div class="graph__choose">
+        <!-- <div class="currency__choose"> -->
+          <span class="currency__choose">Выберите валюту</span>
+          <select name="" id="" class="currency__select">
+            <option value="">Bitcoin (BTC)</option>
+            <option value="">Ethereum (ETH)</option>
+            <option value="">US dollar (USD)</option>
+          </select>
+        <!-- </div> -->
+      </div>
+    </div>
+    <!-- /.graph -->
+    <!-- <router-view/> -->
   </div>
+  <!-- /.app -->
 </template>
 
 <script>
@@ -33,8 +65,15 @@ export default {
   data() {
     return {
       converter: 'Конвертер валют',
-      briefcase: 'Валютный портфель'
+      briefcase: 'Валютный портфель',
+      currencyInput: '',
+      currencyOutput: '',
+      currencySelect: '',
     }
+  },
+
+  methods: {
+    
   }
 }
 </script>
@@ -42,6 +81,8 @@ export default {
 <style lang="scss">
   * {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
   body {
     // background-color: #0b3102cb;
@@ -53,20 +94,10 @@ export default {
     background: linear-gradient(45deg, rgba(16, 17, 15, 0.815) 0%, #0b3102cb 85.62%), url('./img/crypto-bg.jpg') no-repeat center top / cover;
   }
   .header {
-    margin-top: 50px;
-    margin-bottom: 50px;
+    /* margin-top: 50px;
+    margin-bottom: 50px; */
     display: flex;
     justify-content: center;
-  }
-  .header-converter {
-    margin-right: 10px;
-  }
-  .converter-link, .briefcase-link {
-    color: #fff;
-  }
-  .converter-link::after {
-    content: '|';
-    margin-left: 10px;
   }
   .container {
     max-width: 1170px;
@@ -74,36 +105,73 @@ export default {
     // width: 90%;
     display: flex;
     justify-content: center;
-
+  
   }
-  .first {
+  .header__container {
+    margin-top: 50px;
+    margin-bottom: 50px;
+    /* display: flex;
+    justify-content: center; */
+  }
+  .header__converter {
+    margin-right: 10px;
+  }
+  .header__converter::after {
+    content: '|';
+    margin-left: 10px;
+  }
+  .header__link, .header__link {
+    color: #fff;
+  }
+  // header style
+  .currency {
+    margin-bottom: 50px;
+  }
+  .currency__block_first {
     margin-right: 30px;
   }
-  .select {
+  .currency__select {
     cursor: pointer;
     background: rgba(255, 255, 255, 0.35);
     border: 1px solid rgba(255, 255, 255, 0.5);
     color: #fff;
-    padding: 18px;
+    padding: 15px;
     width: 250px;
     border-radius: 10px;
     font-size: 18px;
   }
-  .currency-block {
+  .currency__block {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
   }
-  .currency-choose {
+  .currency__choose {
     margin-bottom: 10px;
   }
-  .currency-input {
+  .currency__input {
     margin-top: 20px;
-    padding: 18px;
+    padding: 15px;
     width: 250px;
     border-radius: 10px;
     font-size: 18px;
   }
+  // currency style
+  .graph {
+    display: flex;
+    flex-direction: column;
+  }
+  .graph__text {
+    margin-bottom: 30px;
+    text-align: center;
+  }
+  .graph__choose {
+    display: flex;
+    flex-direction: column;
+    align-self: start;
+    align-items: center;
+    // justify-content: center;
+  }
+  // graph style
   
 </style>
